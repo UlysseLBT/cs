5b86f0845307bdbea683c615ee8b527614f2830e
UlysseLBT <lebouteiller.ulysse@gmail.com>
2025-09-03 11:45:03 +0200

    feat: cs

---
+++ Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿string message = "Hello, World!";
+Console.WriteLine(message);
+
+int nombre = 42;
+Console.WriteLine(nombre);
+
+double pi = 3.14159;
+Console.WriteLine(pi);
+
+char lettre = 'A';
+Console.WriteLine(lettre);
+
+bool estVrai = true;
+Console.WriteLine(estVrai);
+
+var variableImplicite = "Je suis une chaîne de caractères";
+Console.WriteLine(variableImplicite);
+
