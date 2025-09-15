06f6ca7e59440b3ece77092b1ebf83c54a3b4c09
UlysseLBT <lebouteiller.ulysse@gmail.com>
2025-09-15 12:10:33 +0200

    Expoo

--- EXpoo/Compte.cs
+++ EXpoo/Compte.cs
@@ -1,4 +1,8 @@
+//Ajouter à la classe compte une propriété manuelle Solde
 class Compte
 {
-    
-}
+    // Constructeur par défaut
+    public Compte()
+    {
+        Solde = 0.0;
+    }
--- EXpoo/Program.cs
+++ EXpoo/Program.cs
@@ -1,1 +1,2 @@
 ﻿// Creer une classe Compte dans son propre fichier cs
+//Ajouter à la classe compte une propriété manuelle Solde
